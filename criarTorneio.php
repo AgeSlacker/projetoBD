bc862e927961615e6b294156655ce74b5c2f7fec
AchillesAk <56974278+AchillesAk@users.noreply.github.com>
2019-11-28 19:39:16 +0000

    primeiros passos php criarTorneio

--- criarTorneio.php
+++ criarTorneio.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+
+require_once "connect.php";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
+
+<?php
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -14,97 +31,99 @@
     <link rel="stylesheet" href="assets/css/backgroundflatpickrselect.css">
 </head>
 
-<body style = "overflow-x:hidden;user-select: none;">
-  <?php require "top_navbar.php" ?>
-    <div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div style="margin-top: 0px;margin-bottom: 78px;height: 80px;padding-top: 29px;width: 100%;">
-                        <h1 class="text-center" style="margin-top: 0px;margin-bottom: 0px;">Criação de Torneio</h1>
-                    </div>
-                </div>
-                <div class="col">
-                    <div></div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-3">
-                            <div class="text-center" style="margin-top: 30px;"><i class="fa fa-calendar" style="font-size: 73px;"></i>
-                                <p>Defina o inicio e fim do torneio</p>
-                            </div>
-                        </div>
-                        <div class="col">
-                          <div class="datePicker">
-                            <input id="input" class="input-date" type="text" placeholder="Select Date..." style = "visibility:hidden !important;"data-input>
-                          </div>
+<body style="overflow-x:hidden; user-select: none;">
+    <?php require "top_navbar.php" ?>
+    <form method="post">
+        <div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div style="margin-top: 0px;margin-bottom: 78px;height: 80px;padding-top: 29px;width: 100%;">
+                            <h1 class="text-center" style="margin-top: 0px;margin-bottom: 0px;">Criação de Torneio</h1>
                         </div>
                     </div>
+                    <div class="col">
+                        <div></div>
+                    </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="row">
-                        <div class="col-3 text-center">
-                            <div><i class="fa fa-clock-o" style="font-size: 77px;"></i>
-                                <p>Defina o Horario dos jogos</p>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="text-center" style="margin-top: 30px;"><i class="fa fa-calendar" style="font-size: 73px;"></i>
+                                    <p>Defina o inicio e fim do torneio</p>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="datePicker">
+                                    <input id="input" class="input-date" name="range" type="text" placeholder="Select Date..." style="visibility:hidden !important;" data-input>
+                                </div>
                             </div>
                         </div>
-                        <div class="col">
-                            <div style = "margin-top:40px;">
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Segunda-Feira</label></div>
-                                    </div>
-                                    <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-3 text-center">
+                                <div><i class="fa fa-clock-o" style="font-size: 77px;"></i>
+                                    <p>Defina o Horario dos jogos</p>
                                 </div>
                             </div>
-                            <div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Terça-Feira</label></div>
+                            <div class="col">
+                                <div style="margin-top:40px;">
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" name="seg" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Segunda-Feira</label></div>
+                                        </div>
+                                        <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                     </div>
-                                    <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                 </div>
-                            </div>
-                            <div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Quarta-Feira</label></div>
+                                <div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" name="ter" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Terça-Feira</label></div>
+                                        </div>
+                                        <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                     </div>
-                                    <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                 </div>
-                            </div>
-                            <div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Quinta-Feira</label></div>
+                                <div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" name="qua" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Quarta-Feira</label></div>
+                                        </div>
+                                        <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                     </div>
-                                    <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                 </div>
-                            </div>
-                            <div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Sexta-Feira</label></div>
+                                <div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" name="qui" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Quinta-Feira</label></div>
+                                        </div>
+                                        <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                     </div>
-                                    <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                 </div>
-                            </div>
-                            <div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Sabado</label></div>
+                                <div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" name="sex" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Sexta-Feira</label></div>
+                                        </div>
+                                        <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                     </div>
-                                    <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                 </div>
-                            </div>
-                            <div>
-                                <div class="row">
-                                    <div class="col">
-                                        <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Domingo</label></div>
+                                <div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="form-check" style="margin-bottom: 10px;"><input class="form-check-input" name="sab" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Sabado</label></div>
+                                        </div>
+                                        <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
                                     </div>
-                                    <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
+                                </div>
+                                <div>
+                                    <div class="row">
+                                        <div class="col">
+                                            <div class="form-check"><input class="form-check-input" name="dom" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Domingo</label></div>
+                                        </div>
+                                        <div class="col"><input class="border rounded-0 border-dark" type="time"></div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -112,18 +131,25 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div>
-        <div class="row">
-            <div class="col text-right"><button class="btn btn-dark" type="button" style="background-color: rgb(0,0,0);width: 159px;margin-bottom: 20px;margin-top: 80px;margin-right: 20px;">Cancelar &nbsp;&nbsp;<i class="fa fa-close"></i></button></div>
-            <div class="col"><button class="btn btn-dark" type="button" style="background-color: rgb(0,0,0);width: 159px;margin-top: 80px;margin-right: 20px;">Confirmar &nbsp;&nbsp;<i class="fa fa-check"></i>&nbsp;</button></div>
+        <div>
+            <div class="row">
+                <div class="col text-right"><button class="btn btn-dark" name="cancelar" type="submit" style="background-color: rgb(0,0,0);width: 159px;margin-bottom: 20px;margin-top: 80px;margin-right: 20px;">Cancelar &nbsp;&nbsp;<i class="fa fa-close"></i></button></div>
+                <div class="col"><button class="btn btn-dark" name="criar" type="submit" style="background-color: rgb(0,0,0);width: 159px;margin-top: 80px;margin-right: 20px;">Confirmar &nbsp;&nbsp;<i class="fa fa-check"></i>&nbsp;</button></div>
+            </div>
         </div>
-    </div>
+    </form>
     <?php require "footer.php" ?>
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flatpickr/4.2.3/flatpickr.js"></script>
-    <script >$(".input-date").flatpickr({inline:true,mode: "range",dateFormat: "d-m-Y",minDate: "today"});</script>
+    <script>
+        $(".input-date").flatpickr({
+            inline: true,
+            mode: "range",
+            dateFormat: "d-m-Y",
+            minDate: "today"
+        });
+    </script>
 </body>
 
 </html>
