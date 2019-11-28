faa3c70501ce2f2b79125fd9c5f28b41fc8d2633
Olex <30861670+Olex@users.noreply.github.com>
2019-11-28 17:38:05 +0000

    mudei

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-//
+// mudou
 ?>
 
 <!DOCTYPE html>
