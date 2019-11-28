4d8097f32830394d3ac9a7b0d4ee237542bcb7a7
AchillesAk <56974278+AchillesAk@users.noreply.github.com>
2019-11-28 20:01:23 +0000

    logo icon

--- top_navbar.php
+++ top_navbar.php
@@ -6,7 +6,7 @@
 <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
 <link rel="stylesheet" href="assets/css/styles.css">
 <nav class="navbar navbar-light navbar-expand-md navigation-clean" style="background-color: rgb(0,0,0);">
-    <div class="container-fluid"><a class="navbar-brand" href="index.php" style="color: rgb(255,255,255);">DBola</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+    <div class="container-fluid"><a class="navbar-brand" href="index.php" style="color: rgb(255,255,255);"><img src="assets/images/icon.png" style="margin-right:15px;width:50px;height:50px;filter:invert(100%);">DBola</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="nav navbar-nav ml-auto">
                 <li class="nav-item" role="presentation"><a class="nav-link" href="#" style="color: rgb(255,255,255);">Quero Jogar</a></li>
