349537e7641e2df08d700378d4d7215aa1b53148
Olex <30861670+Olex@users.noreply.github.com>
2019-11-28 17:50:16 +0000

    db connection in cpnnect.php

--- connect.php
+++ connect.php
@@ -4,3 +4,8 @@ $port = 3306;
 $username = "qm3jt7qs_AIOO";
 $password = "basesdedados";
 $dbname = "qm3jt7qs_projetobd_aioo";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
