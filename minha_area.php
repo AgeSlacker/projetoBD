7e288e705ced67a78791df81c0bf3c20be3db37c
osierpe <45239912+osierpe@users.noreply.github.com>
2019-11-30 11:11:47 +0000

    Add files via upload

--- minha_area.php
+++ minha_area.php
@@ -1,7 +1,15 @@
 <?php
 session_start();
-require "force_login.php";
-require_once "connect.php";
+
+?>
+
+
+<?php require_once "connect.php";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 
 ?>
 <html>
@@ -22,24 +30,23 @@ require_once "connect.php";
     <div class="row">
         <div class="col">
             <h1 class="text-center">Minha Área ( <?php
-                                                    $cc = $_SESSION["cc"];
-                                                    $pesquisa = "SELECT nome
+				$cc = $_SESSION["cc"];
+				$pesquisa = "SELECT nome
 						FROM pessoa
 						WHERE cc = '$cc'";
-                                                    if ($result = $conn->query($pesquisa)) {
-
-                                                        $row = $result->fetch_assoc();
-                                                        $nomelog = $row["nome"];
-
-                                                        echo $nomelog;
-                                                    }
-                                                    ?>)</h1>
+				if ($result= $conn->query($pesquisa)){
+					
+					$row = $result->fetch_assoc();
+					$nomelog = $row["nome"];
+					
+				echo $nomelog;}
+						?>)</h1>
         </div>
     </div>
     <div class="row" style="height: 15%;">
         <div class="col-md-3">
             <ul class="nav nav-tabs" style="width: 100%;color: rgb(255,255,255);">
-                <li class="nav-item" style="width: 100%;color: rgb(255,255,255);background-color: #000000;"><a class="nav-link active d-xl-flex justify-content-xl-center align-items-xl-center" href="#" style="color: rgb(255,255,255);background-color: rgb(0,0,0);">Perfil</a></li>
+                <li class="nav-item" style="width: 100%;color: rgb(255,255,255);background-color: #000000;"><a class="nav-link active d-xl-flex justify-content-xl-center align-items-xl-center" href="Meu_perfil.php" style="color: rgb(255,255,255);background-color: rgb(0,0,0);">Perfil</a></li>
             </ul>
         </div>
         <div class="col-md-3">
@@ -49,7 +56,7 @@ require_once "connect.php";
         </div>
         <div class="col-md-3">
             <ul class="nav nav-tabs" style="width: 100%;">
-                <li class="nav-item" style="width: 100%;"><a class="nav-link active d-xl-flex justify-content-xl-center align-items-xl-center" href="#" style="background-color: rgb(0,0,0);color: rgb(255,255,255);">Jogos</a></li>
+                <li class="nav-item" style="width: 100%;"><a class="nav-link active d-xl-flex justify-content-xl-center align-items-xl-center" href="Meus_jogos.php" style="background-color: rgb(0,0,0);color: rgb(255,255,255);">Jogos</a></li>
             </ul>
         </div>
         <div class="col-md-3">
@@ -63,9 +70,42 @@ require_once "connect.php";
             <div class="row" style="margin-top: 15%;width: 1134px;height: 210px;">
                 <div class="col-4 col-sm-4 col-md-4 offset-md-2 offset-lg-3 offset-xl-4" style="background-color: #dedede;">
                     <h2 class="text-center">Estatísticas</h2>
-                    <p style="margin-top: 26px;">Torneios Participados:</p>
-                    <p>Jogos Participados:</p>
-                    <p>Golos Marcados:</p>
+                    <p style="margin-top: 26px;">Torneios Participados: <?php	$cc = $_SESSION["cc"];
+											$pesquisa = "SELECT *
+														FROM torneio_jogador
+														WHERE pessoa_cc = '$cc'";
+											
+											if($result= $conn->query($pesquisa)){
+												$count_torn = $result->num_rows;
+												echo $count_torn;
+											}
+											else{echo "error in sql syntax";}
+											
+					?></p>
+                    <p>Jogos Participados: <?php	$cc = $_SESSION["cc"];
+											$pesquisa = "SELECT *
+														FROM posjogadorequipa as p, jogo
+														WHERE p.equipa_nome = jogo.equipa_nome OR p.equipa_nome = jogo.equipa_nome1
+														AND p.pessoa_cc = '$cc'";
+											if($result= $conn->query($pesquisa)){
+												$count_jog = $result->num_rows;
+												echo $count_jog;
+											}
+											
+											else{echo "error in sql syntax";}
+					?></p>
+                    <p>Golos Marcados: <?php	$cc = $_SESSION["cc"];
+											$pesquisa = "SELECT *
+														FROM golos
+														WHERE pessoa_cc = '$cc' AND valido = 1";
+											
+											if($result= $conn->query($pesquisa)){
+												$count_golos = $result->num_rows;
+												echo $count_golos;
+											}
+											
+											else{echo "error in sql syntax";}
+					?></p>
                 </div>
             </div>
         </div>
